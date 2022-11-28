dc64635f8bf6af9f8018cb63ff9fbab13fc489f5
LionheartIV <54432930+LionheartIV@users.noreply.github.com>
2022-11-28 13:39:12 +0100

    Update main.dart

--- first_flutter/lib/main.dart
+++ first_flutter/lib/main.dart
@@ -1,16 +1,70 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   TestClass tc = TestClass(id: 1000, name: "Phil");
+//
+//   print('Id:  ${tc.id}');
+//   print("Name: " + tc.name.toString());
+// }
+//
+// class TestClass {
+//   int? _id;
+//   String? _name;
+//
+//   int? get id => _id;
+//   String? get name => _name;
+//
+//   set id(int? value) {
+//     _id = value;
+//   }
+//   set name(String? value) {
+//     _name = value;
+//   }
+//
+//   TestClass({ int? id,String? name}){
+//     this.id = _id;
+//     this.name = _name;
+//   }
+// }
 void main() {
-  TestClass tc = TestClass(1000, "Philip");
-
-  print('Id:  ${tc.id}');
-  print("Name: " + tc.name);
+  runApp(const MyApp());
 }
 
-class TestClass {
-  int id;
-  String name;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  TestClass(this.id, this.name);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Expanded Widget',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Expanded Widget'),
+            ),
+            body: Column(children: [
+              Container(
+                  height: 200,
+                  color: Colors.green[600],
+                  child: const Center(
+                      child: Text('200', style: TextStyle(fontSize: 50)))),
 
+              Container(
+                  height: 150,
+                  color: Colors.red[600],
+                  child: const Center(
+                      child:
+                      Text('Expanded', style: TextStyle(fontSize: 50)))),
+
+              Container(
+                  height: 400,
+                  color: Colors.blue[600],
+                  child: const Center(
+                      child: Text('400', style: TextStyle(fontSize: 50)))),
+            ])
+        )
+    );
+  }
 }
